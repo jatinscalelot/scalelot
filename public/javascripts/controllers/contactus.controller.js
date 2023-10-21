997609fc556baa15f76661b3b3e4d48fb621e45a
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-10-21 12:40:31 +0530

    update contact controller

--- public/javascripts/controllers/contactus.controller.js
+++ public/javascripts/controllers/contactus.controller.js
@@ -1,5 +1,4 @@
 app.controller("contactUsController", ($scope, $http) => {
-    $scope.code = "+91";
     $scope.form = {
         name : '',
         email : '',
@@ -11,6 +10,30 @@ app.controller("contactUsController", ($scope, $http) => {
     $scope.onContact = () => {
         var dialcode = $('.iti__selected-dial-code').text();
         $scope.form.contrycode = dialcode;
-        console.log('form data', $scope.form);
+        if($scope.form && $scope.form != null && $scope.form != undefined){
+            if($scope.form.name && $scope.form.name != '' && $scope.form.name.trim() != '' && $scope.form.name != undefined){
+                if($scope.form.email && $scope.form.email != '' && $scope.form.email.trim() != '' && $scope.form.email != undefined && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.form.email))){
+                    if($scope.form.phone && $scope.form.phone != '' && $scope.form.phone.trim() != '' && $scope.form.phone.trim().length == 10 && (/^[0-9]+$/.test($scope.form.phone))){
+                        if($scope.form.subject && $scope.form.subject != '' && $scope.form.subject != 'Subject'){
+                            if($scope.form.message && $scope.form.message != '' && $scope.form.message.trim() != ''){
+                                
+                            }else{
+                                swal('Message content can not be empty, please try again...', { icon: "error" });
+                            }
+                        }else{
+                            swal('Please select subject, and try again...', { icon: "error" });
+                        }
+                    }else{
+                        swal('Invalid contact number, Please enter valid contact number and try again...', { icon: "error" });
+                    }
+                }else{
+                    swal('Invalid email-id, Please enter valid email-id and try again...', { icon: "error" });
+                }
+            }else{
+                swal('Name can not be empty, Please eneter your good name and try again...', { icon: "error" });
+            }
+        }else{
+            swal('Invalid data to contact with us, please try again...', { icon: "error" });
+        }
     }
 });
