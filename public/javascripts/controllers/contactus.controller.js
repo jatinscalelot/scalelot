55d6c23ddb1b3b8d37318dcf89ae121af38af07a
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-10-21 11:54:20 +0530

    update contact controller

--- public/javascripts/controllers/contactus.controller.js
+++ public/javascripts/controllers/contactus.controller.js
@@ -2,8 +2,7 @@ app.controller("contactUsController", ($scope, $http) => {
     $scope.code = "+91";
     $scope.onContact = () => {
         var dialcode = $('.iti__selected-dial-code').text();
-        console.log('dialcode', dialcode);
+        $scope.form.contrycode = dialcode;
         console.log('form data', $scope.form);
-        console.log('tel', $scope.tel);
     }
 });
