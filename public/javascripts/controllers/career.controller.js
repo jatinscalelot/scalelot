a0367b0809348d0f1a81bc0913281e3160ff2c84
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-10-23 18:07:24 +0530

    update about us and career angular code

--- public/javascripts/controllers/career.controller.js
+++ public/javascripts/controllers/career.controller.js
@@ -1,7 +1,7 @@
 app.controller("careerController", ($scope, $http) => {
     $scope.email = '';
     $scope.selectedPosition = '';
-    $scope.onCreereClick = () => {
+    $scope.onCreereClick = (position) => {
         $scope.selectedPosition = position;
         $('#careerModal').model('show');
     }
