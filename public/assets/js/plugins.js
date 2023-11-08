825fa1a045e9b7839f1149a61a855b80979abb2d
dprashant <dprashant.scalelot@gmail.com>
2023-11-08 14:33:37 +0530

    latest_update2:53

--- public/assets/js/plugins.js
+++ public/assets/js/plugins.js
@@ -145,7 +145,7 @@
 				infinite: true,
 				autoplay: true,
 				focusOnSelect: true,
-				slidesToShow: 6,
+				slidesToShow: 5,
 				speed: 1000,
 				autoplaySpeed: 3000,
 				slidesToScroll: 1,
@@ -536,9 +536,9 @@
 				arrows: false,
 				dots: false,
 				responsive: [{
-						breakpoint: 1200,
+						breakpoint: 1024,
 						settings: {
-							slidesToShow: 4,
+							slidesToShow: 3,
 						},
 					},
 					{
@@ -550,7 +550,7 @@
 					{
 						breakpoint: 576,
 						settings: {
-							slidesToShow: 2,
+							slidesToShow: 1,
 						},
 					},
 					{
